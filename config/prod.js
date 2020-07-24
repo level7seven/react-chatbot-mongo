01193fa418f13296cfbce7408d908d40c5af0bfa
level7seven <level7seven@outlook.com>
2020-07-24 12:20:38 +0200

    chatbot with keys, connect server side and dialogflow fixed

--- config/prod.js
+++ config/prod.js
@@ -4,6 +4,6 @@ module.exports = {
     dialogFlowSessionID: process.env.DIALOGFLOW_SESSION_ID,
     dialoFlowSessionLanguageCode: process.env.DIALOGFLOW_LANGUAGE_CODE,
     googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
-    googlePrivateKey: JSON.parse(process.env.GOOGLE_PRIVATE_KEY)
+    googlePrivateKey: (process.env.GOOGLE_PRIVATE_KEY)
 }
 
